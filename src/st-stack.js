ee0eb02acd9d33d5d6e63970d667df5e09229467
nikitsch <79355999+nikitsch@users.noreply.github.com>
2021-10-18 19:44:06 +0300

    feat: add code

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.array = [];
+  }
+
+  push(element) {
+    this.array.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1];
   }
 }
